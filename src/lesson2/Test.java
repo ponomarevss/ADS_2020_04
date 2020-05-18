e52282641f3cf7adb21c2c7eb0a25d9de78a6035
ponomarevss <s-siberia@yandex.ru>
2020-05-18 13:27:39 +0500

    выполнил сравнение методов сортировки массивов

--- src/lesson2/Test.java
+++ src/lesson2/Test.java
@@ -2,19 +2,35 @@ package lesson2;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.function.Supplier;
 
 public class Test {
 
+    private static final int ARRAY_SIZE = 10_000;
+
     public static void main(String[] args) {
 
-        int size = 10;
-        Array<Integer> initialArray = new ArrayImpl(size);
-        for (int i = 0; i < size; i++) {
-            initialArray.add(new Random().nextInt(20));
+        Integer[] initials = new Integer[ARRAY_SIZE];
+        for (int i = 0; i < initials.length; i++) {
+            initials[i] = new Random().nextInt();
         }
-        initialArray.display();
-        //TODO: допилить метод создания копии массива, выполнить основное задание
+
+        Array<Integer> arr1 = new ArrayImpl(Arrays.copyOf(initials, initials.length));
+        Array<Integer> arr2 = new ArrayImpl(Arrays.copyOf(initials, initials.length));
+        Array<Integer> arr3 = new ArrayImpl(Arrays.copyOf(initials, initials.length));
+
+        long start;
+        start = System.currentTimeMillis();
+        arr1.sortBubble();
+        System.out.println((System.currentTimeMillis() - start) + " - sortBubble");
+
+        start = System.currentTimeMillis();
+        arr2.sortSelect();
+        System.out.println((System.currentTimeMillis() - start) + " - sortSelection");
+
+        start = System.currentTimeMillis();
+        arr3.sortInsert();
+        System.out.println((System.currentTimeMillis() - start) + " - sortInsert");
 
     }
+
 }
